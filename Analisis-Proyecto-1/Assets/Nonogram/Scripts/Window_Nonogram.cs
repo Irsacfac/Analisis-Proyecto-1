e07f44bcfab23b7c44e511e771fabba02cc27ede
Irsacfac <irsacfac@gmail.com>
2020-03-01 21:59:15 -0600

    Se organizo el programa

---
+++ Analisis-Proyecto-1/Assets/Nonogram/Scripts/Window_Nonogram.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Window_Nonogram : MonoBehaviour
+{
+
+    private RectTransform nonogramContainer;
+
+    private void Awake(){
+        nonogramContainerContainer = transfor.Find("nonogramContainer").GetComponent<RectTransform>();
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
