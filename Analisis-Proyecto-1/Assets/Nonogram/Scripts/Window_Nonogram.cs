a376110ea3679abbaf636962cbe781ecfa090ee0
Irsacfac <irsacfac@gmail.com>
2020-03-02 19:30:59 -0600

    Lectura del archivo creado
    Se lee el archivo creado

--- Analisis-Proyecto-1/Assets/Nonogram/Scripts/Window_Nonogram.cs
+++ Analisis-Proyecto-1/Assets/Nonogram/Scripts/Window_Nonogram.cs
@@ -8,10 +8,12 @@ public class Window_Nonogram : MonoBehaviour
 
     private RectTransform nonogramContainer;
     private TextWriter archivo;
+    private TextReader lectorArchivo;
 
     private void Awake(){
         nonogramContainer = transform.Find("nonogramContainer").GetComponent<RectTransform>();
-        creartxt();
+        //creartxt();
+        leertxt();
     }
 
     // Start is called before the first frame update
@@ -36,5 +38,17 @@ public class Window_Nonogram : MonoBehaviour
 
         //cerrar un archivo
         archivo.Close();
+    }
+
+    private void leertxt(){
+        //abrir en modo solo lectura?
+        lectorArchivo = new StreamReader("archivo.txt");
+        //escribir en la consola de Unity
+        Debug.Log(lectorArchivo.ReadLine());//lee una linea linea
+        Debug.Log(lectorArchivo.ReadLine());//cada vez que se llama se lee la linea siguiente
+        Debug.Log(lectorArchivo.ReadToEnd());//lee lodo el archivo, si se leyeron lineas con anterioridad empezara en la linea siguiente a la ultima leida
+        //si no hay nada escrito, o no resta nada por leer, retorna "" o vacio. No lo se bien
+        //cerrar archivo
+        lectorArchivo.Close();
     }
 }
