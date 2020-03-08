8a73de8dc7d386fba0b891395eea7834e26e8162
KeylorCalderon <43432543+KeylorCalderon@users.noreply.github.com>
2020-03-08 01:21:00 -0600

    Lectura del archivo

--- Analisis-Proyecto-1/Assets/Nonogram/Scripts/Window_Nonogram.cs
+++ Analisis-Proyecto-1/Assets/Nonogram/Scripts/Window_Nonogram.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
+using System;
 
 public class Window_Nonogram : MonoBehaviour
 {
@@ -12,19 +13,21 @@ public class Window_Nonogram : MonoBehaviour
     [SerializeField] private Sprite cRellenaSprite;
     private TextWriter archivo;
     private TextReader lectorArchivo;
+    private int X;
+    private int Y;
 
     private void Awake(){
-        nonogramContainer = transform.Find("nonogramContainer").GetComponent<RectTransform>();
-        //creartxt();
+        //nonogramContainer = transform.Find("nonogramContainer").GetComponent<RectTransform>();
+        creartxt();
         leertxt();
         //dibujarCuadriculas(new Vector2(200,200));
-        dibujarNonogram(24,38);//max actual(con valores anclados 24x38)
+        //dibujarNonogram(24,38);//max actual(con valores anclados 24x38)
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -47,12 +50,54 @@ public class Window_Nonogram : MonoBehaviour
 
     private void leertxt(){
         //abrir en modo solo lectura?
-        lectorArchivo = new StreamReader("archivo.txt");
+        lectorArchivo = new StreamReader("Nonogram1.txt");
         //escribir en la consola de Unity
-        Debug.Log(lectorArchivo.ReadLine());//lee una linea linea
+
+        String linea=lectorArchivo.ReadLine();           //
+        linea = linea.Replace(" ","");                   // Lee y guarda
+        int separador = linea.IndexOf(",");              // el largo y ancho
+        X = int.Parse(linea.Substring(0, separador));    // del Nonogram
+        Y = int.Parse(linea.Substring(separador+1));     //
+        Debug.Log("X: "+X+" Y: "+Y);
+
+        bool FilaColumna=false;
+        do
+        {
+            linea = lectorArchivo.ReadLine();
+            if (linea != null)
+            {
+                if (linea.Equals("FILAS"))
+                {
+                    FilaColumna = true;
+                }
+                else if (linea.Equals("COLUMNAS"))
+                {
+                    FilaColumna = false;
+                }
+                else
+                {
+                    linea = linea.Replace(" ", "");
+                    string[] separadas;
+                    separadas = linea.Split(',');
+                    if (FilaColumna)
+                    {
+                        //Añadir a la lista de filas
+                        Debug.Log("Fila: " + separadas.Length);
+                    }
+                    else
+                    {
+                        //Añadir a la lista de columnas
+                        Debug.Log("Columnas: " + separadas.Length);
+                    }
+                }
+            }
+            
+        } while (linea != null);
+
+        /*Debug.Log(lectorArchivo.ReadLine());//lee una linea linea
         Debug.Log(lectorArchivo.ReadLine());//cada vez que se llama se lee la linea siguiente
         Debug.Log(lectorArchivo.ReadToEnd());//lee lodo el archivo, si se leyeron lineas con anterioridad empezara en la linea siguiente a la ultima leida
-        //si no hay nada escrito, o no resta nada por leer, retorna "" o vacio. No lo se bien
+        //si no hay nada escrito, o no resta nada por leer, retorna "" o vacio. No lo se bien*/
         //cerrar archivo
         lectorArchivo.Close();
     }
